5ef2a8f84a9fde62964399ef434746e0dbfebe95
S33156 <109063419+S33156@users.noreply.github.com>
2023-08-23 19:53:38 +0530

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,21 @@
+import sys
+import time
+import random
+
+import os
+import shutil
+
+a = 33
+b = 33
+
+if b > a:
+    print("B is greater than a")
+elif a == b:
+    print("A and B are equal")
+
+def my_function(fname):
+    print(fname + " Refsnes")
+
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
